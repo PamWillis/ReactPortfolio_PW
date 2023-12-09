6766f4f069c54b13e467a98c775ab3c3de1a84f5
Pam <pwillisop@yahoo.com>
2023-12-09 14:12:44 -0500

    resolved navigator

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 // Here we are importing a CSS file as a dependency
+import { Outlet } from 'react-router-dom';
 import '../styles/Header.css';
-import './NavTabs';
+import NavTabs from './NavTabs';
 
 function Header() {
   return (
     <header className="header">
         <h1>Pam Willis</h1>
-      <nav>{NavTabs}</nav>
+      <nav><NavTabs/></nav>
     </header>
   );
 }
